6d7491174fc046b1a44db39f8f98d1c88c80232c
lakshmikanth-472 <lakshmikanthbhat472@gmail.com>
2022-09-20 13:32:26 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,61 @@ children:<Widget> [
 Container(
   alignment:Alignment.centerRight,
   padding: EdgeInsets.fromLTRB(10,20, 10, 0),
+  child: Text("0",style: TextStyle(fontSize: 38),),
   
 
-)
+),
+Container(
+  alignment:Alignment.centerRight,
+  padding: EdgeInsets.fromLTRB(10,30, 10, 0),
+  child: Text("0",style: TextStyle(fontSize: 48),),//To display the zero on the screen
+  
+
+),
+//Dividing the screen into halves
+Expanded //Top part of the screen
+(child:Divider()
+
+
+
+
+),
+//Bottom part
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: <Widget>[
+    Container(
+      width: MediaQuery.of(context).size.width*.75,//Covering the 75 percent of the screen 
+      child: Table(
+          children: [
+            TableRow(
+              children: [
+                Container(
+
+                    height: MediaQuery.of(context).size.height*0.1,
+                    color: Colors.redAccent,
+                    child:FlatButton()
+
+
+                ),
+              ],
+            ),
+
+
+
+          ],
+      ),
+    ),
+
+
+
+  ],
+),
+
+
+
+
+
 ],
 
 
